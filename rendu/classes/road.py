b8439a4ab10fabb4c6cf4a470969241ac76bd487
Oceane <sejourne.oceane@gmail.com>
2020-04-22 10:50:45 +0200

    Create get_neighbours method

--- rendu/classes/road.py
+++ rendu/classes/road.py
@@ -9,11 +9,37 @@ class Road:
 
 
 class RoadNetwork:
-    def __init__(self):
-        print('todo')
+    def __init__(self, road_list):
+        self.road_list = road_list
 
     def get_neighbours(self, intersection):
-        print('renvoie liste des intersections voisines')
+        neighbours = []
+
+        for item in self.road_list:
+            if item.intersection1 == intersection:
+                neighbours.append(item.intersection2)
+
+        return neighbours
 
     def get_distance(self, intersection1, intersection2):
         print('renvoie leur distance')
+
+
+Network = RoadNetwork([
+    Road(0, 1, 4),
+    Road(0, 7, 8),
+    Road(1, 7, 11),
+    Road(1, 2, 8),
+    Road(7, 8, 7),
+    Road(7, 6, 1),
+    Road(2, 8, 2),
+    Road(8, 6, 6),
+    Road(2, 3, 7),
+    Road(2, 5, 4),
+    Road(5, 6, 2),
+    Road(3, 5, 14),
+    Road(3, 4, 9),
+    Road(4, 5, 10)
+])
+
+intersection0_neighbours = Network.get_neighbours(0)
